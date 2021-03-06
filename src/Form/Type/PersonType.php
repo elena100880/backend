dccbfb13338f02a2540d7e9437dec0fbcdae4f76
elena100880 <100880kr@gmail.com>
2021-03-06 11:23:38 +0100

    some small changes in code view

--- src/Form/Type/PersonType.php
+++ src/Form/Type/PersonType.php
@@ -10,6 +10,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 use Symfony\Component\Form\Form;
